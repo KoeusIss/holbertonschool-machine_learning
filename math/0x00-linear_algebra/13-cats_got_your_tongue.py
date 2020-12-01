27dd27329a0d517af6c32607de08456f7a584db7
KoeusIss <sebri.issam@gmail.com>
2020-12-01 15:09:38 +0100

    fix the docstring

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -7,20 +7,21 @@ def np_cat(mat1, mat2, axis=0):
     """Concatenates two matrices along a specefic axis
 
     Args:
-        mat1 (list[list]): the first given matrix
-        mat2 (list[list]): the second ginve matrix
+        mat1 (numpy.ndarray): the first given matrix
+        mat2 (numpy.ndarray): the second ginve matrix
         axis (int(0|1)): the given specific axis if axis == 0 so we need
         concatenates the two matrices along a column (verticaly) axis, else
         it must concatenated along the column axis (horizentaly)
     Returns:
-        (list[list]|None): return the already concatenated new matrix, if the
+        (numpy.ndarray): return the already concatenated new matrix, if the
         two matrix is uncompatible for concatenation, it should return None
     Example:
-        mat1 = [[1, 2], [3, 4]]
-        mat2 = [[5, 6]]
-        mat3 = [[7], [8]]
-        cat_matrices2D(mat1, mat2) -> [[1, 2], [3, 4], [5, 6]
-        cat_matrices2D(mat1, mat3, axis=1) -> [[1, 2, 7], [3, 4, 8]]
+        mat1 = np.array([[11, 22, 33], [44, 55, 66]])
+        mat2 = np.array([[1, 2, 3], [4, 5, 6]])
+        np_cat(mat1, mat2) -> [[11 22 33]
+                                [44 55 66]
+                                [ 1  2  3]
+                                [ 4  5  6]]
 
     """
     return np.concatenate((mat1, mat2), axis)
