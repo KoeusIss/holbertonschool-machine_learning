d3798ef7d0597941a837bd3418a9049f5cdc8794
KoeusIss <sebri.issam@gmail.com>
2020-12-08 13:37:28 +0100

    commit the sum calculation

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Sigma notation"""
+
+
+def summation_i_squared(n):
+    """Finds the summation of i squared where i goes from 1 to n
+
+    Args:
+        n (int): the stopping condition integer
+
+    Returns:
+        (int|None): The integer value of the sum, if n is not a valid number
+        return None
+
+    """
+    if n < 0 or not isinstance(n, int):
+        return None
+    return int((n * (n + 1) * (2 * n + 1)) / 6)
