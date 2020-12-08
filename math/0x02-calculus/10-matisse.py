07ccfd366d7b3cdf1118dce939ccc6bb2157ec48
KoeusIss <sebri.issam@gmail.com>
2020-12-08 14:28:07 +0100

    commit the derivative calculater

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Derivatives modules"""
+
+
+def poly_derivative(poly):
+    """Calculates the derivative of a polynomial
+
+    Args:
+        poly (list): the list of coeficient representing a polynomial
+
+    Returns:
+        (list|None): returns a new list of coeficient representing
+        the derivative of the polynomial, if poly is not valid return None.
+
+    """
+    if not isinstance(poly, list):
+        return None
+    if len(poly) == 1:
+        return [0]
+    return [coef * el for coef, el in enumerate(poly) if coef]
