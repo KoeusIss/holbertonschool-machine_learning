b41a723944260d7c594a01a85fe209358a046c81
KoeusIss <sebri.issam@gmail.com>
2020-12-15 18:02:58 +0100

    Fix the return float variable

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -25,14 +25,13 @@ class Poisson:
         if not data:
             if lambtha <= 0:
                 raise ValueError('lambtha must be a positive value')
-            else:
-                self.lambtha = float(lambtha)
+            self.lambtha = float(lambtha)
         else:
             if not isinstance(data, list):
                 raise TypeError('data must be a list')
-            if len(data) <= 2:
+            if len(data) < 2:
                 raise ValueError('data must contain multiple values')
-            self.lambtha = sum(data) / len(data)
+            self.lambtha = float(sum(data) / len(data))
 
     def pmf(self, k):
         """Calculates the value of PMF for a given number of ``successes``
