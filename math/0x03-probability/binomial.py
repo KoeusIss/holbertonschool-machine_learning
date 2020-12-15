3dbd7a7d3810b33a6b705f4013536d4e1887807c
KoeusIss <sebri.issam@gmail.com>
2020-12-15 20:29:10 +0100

    Fix some interval issue

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -25,9 +25,9 @@ class Binomial:
     def __init__(self, data=None, n=1, p=0.5):
         """Initializer"""
         if data is None:
-            if n <= 0:
+            if n < 0:
                 raise ValueError('n must be a positive value')
-            if not 0 < p < 1:
+            if not 0 <= p <= 1:
                 raise ValueError('p must be greater than 0 and less than 1')
             else:
                 self.n = float(n)
