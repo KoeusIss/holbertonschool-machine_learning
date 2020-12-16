cd468c2fccdfe7c49d4904d28edd379aec243664
KoeusIss <sebri.issam@gmail.com>
2020-12-16 12:29:09 +0100

    implement the PMF for binomial distribution

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -45,3 +45,49 @@ class Binomial:
             self.p = 1 - variance / mean
             self.n = round(mean / self.p)
             self.p = mean / self.n
+
+    def pmf(self, k):
+        """Calculates the PMF of the given number of success
+
+        Args:
+            k (int): Is the given number of success
+
+        Returns:
+            float|0: Returns the PMF value for x, or 0 if k is out of range
+
+        """
+        if k < 0:
+            return 0
+        if not isinstance(k, int):
+            k = int(k)
+        return Binomial.combination(self.n, k)\
+            * self.p**k * (1 - self.p)**(self.n - k)
+
+    @staticmethod
+    def factorial(k):
+        """Calculates the factorial of a given value
+
+        Args:
+            k (int): the given integer k
+        Returns:
+            (int): the factoriam of the given k integer
+
+        """
+        fact = 1
+        for i in range(1, k + 1):
+            fact *= i
+        return fact
+
+    @staticmethod
+    def combination(n, k):
+        """Calculates the binomial coefficient for n choose k
+
+        Args:
+            n (int): the geiven integer n
+            k (int): the given integer k
+        Returns:
+            (int): Returns the binomial coefficient
+
+        """
+        return Binomial.factorial(n)\
+            / (Binomial.factorial(k) * Binomial.factorial(n - k))
