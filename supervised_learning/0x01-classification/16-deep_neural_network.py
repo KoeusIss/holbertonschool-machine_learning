7d7a1e04d5778caa4df14d106d829ce790ce42e9
KoeusIss <sebri.issam@gmail.com>
2020-12-31 17:49:51 +0100

    commit the first class for deep neural network

---
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Deep Neural Network module"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """DeepNeural Network class that defines a neural network performing binary
+    classification.
+
+    Attributes:
+        nx (int): Is the number of input features.
+        layers (list): Is the list contains the lenght of the network layers
+        L (int): the number of layers in the neural network
+        cache (dict): Holds all the intermediary values of the network
+        weights (dict): Holds all the weights and biased of the network.
+
+    Raises:
+        TypeError: If nx is not an integer
+        ValueError: If nx less than 1
+        TypeError: If layers is not a list on positive integer
+
+    """
+    def __init__(self, nx, layers):
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if not isinstance(layers, list):
+            raise TypeError('layers must be a list of positive integers')
+        if any(map(lambda x: x < 0 or not isinstance(x, int), layers)):
+            raise TypeError('layers must be a list of positive integers')
+        self.nx = nx
+        self.layers = layers
+        self.L = len(layers)
+        self.cache = dict()
+        self.weights = dict()
+        lst = layers.copy()
+        lst.insert(0, self.nx)
+        for l in range(1, self.L + 1):
+            self.weights['W' + str(l)] = np.random.randn(
+                lst[l], lst[l - 1]) * np.sqrt(2 / lst[l - 1])
+            self.weights['b' + str(l)] = np.zeros((lst[l], 1))
