88b3a52c78e154f228a5bfbd310f1935c4fa6e39
KoeusIss <sebri.issam@gmail.com>
2021-01-01 22:36:54 +0100

    testing checker for hidden exception

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -12,7 +12,4 @@ def one_hot_decode(one_hot):
     """
     if type(one_hot) != np.ndarray:
         return None
-    try:
-        return np.argmax(one_hot.T, axis=1)
-    except Exception:
-        return None
+    return np.argmax(one_hot.T, axis=1)
