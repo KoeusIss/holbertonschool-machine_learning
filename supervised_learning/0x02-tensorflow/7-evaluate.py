d3ac363a3ea26f5231485f9ec038153472b48ebd
KoeusIss <sebri.issam@gmail.com>
2021-01-11 19:38:08 +0100

    Update the return statemnt outside of session stream

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -18,9 +18,11 @@ def evaluate(X, Y, save_path):
     with tf.Session() as session:
         saver = tf.train.import_meta_graph("{}.meta".format(save_path))
         saver.restore(session, save_path)
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
-        y_pred = tf.get_collection('y_pred')[0]
-        accuracy = tf.get_collection('accuracy')[0]
-        loss = tf.get_collection('loss')[0]
-        return session.run([y_pred, accuracy, loss], feed_dict={x: X, y: Y})
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
+        y_pred, accuracy, loss =  session.run([y_pred, accuracy, loss],
+                                              feed_dict={x: X, y: Y})
+    return y_pred, accuracy, loss
