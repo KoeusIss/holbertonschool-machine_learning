Fix the data input flow between layer, normalization and activation

supervised_learning/0x03-optimization/14-batch_norm.py
@@ -17,13 +17,18 @@ def create_batch_norm_layer(prev, n, activation):
         tf.Tensor: Is the activated output of the layer
 
     """
-    gamma = tf.Variable(tf.constant(1.0), name="gamma", trainable=True)
-    beta = tf.Variable(tf.constant(0.0), name="beta", trainable=True)
-    mean, variance = tf.nn.moments(prev, axes=0)
-    epsilon = tf.constant(1e-8)
-    normed = tf.nn.batch_normalization(
-        prev, mean, variance, beta, gamma, epsilon
-    )
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.layers.Dense(n, activation, kernel_initializer=init)
-    return layer(normed)
+    layer = tf.layers.Dense(n, kernel_initializer=init)
+    layered = layer(prev)
+    gamma = tf.Variable(
+        tf.constant(1.0, shape=[n]), name="gamma", trainable=True
+    )
+    beta = tf.Variable(
+        tf.constant(0.0, shape=[n]), name="beta", trainable=True
+    )
+    mean, variance = tf.nn.moments(layered, axes=[0])
+    epsilon = 1e-8
+    normed = tf.nn.batch_normalization(
+        layered, mean, variance, beta, gamma, epsilon
+    )
+    return activation(normed)