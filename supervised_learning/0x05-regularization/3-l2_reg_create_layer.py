5b5fa99d257c3d0663ba7ac4eaab6b804426134b
KoeusIss <sebri.issam@gmail.com>
2021-01-20 11:08:52 +0100

    commit the regularized layer

---
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Regularization module"""
+import tensorflow as tf
+import numpy as np
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """Creates a tensorflow layer that includes L2 regularization
+
+    Args:
+        prev (tf.Tensor): Is containing the output of the previous layer.
+        n (int): Is the number of nodes the new layer should contain
+        activation: Is the activation function that should be used on the layer
+        lambtha (float): Is the l2 regularization parameter
+
+    Returns:
+        tf.layer: The output of the new layer
+
+    """
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    reg = tf.contrib.layers.l2_regularizer(lambtha)
+    layer = tf.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=init,
+        kernel_regularizer=reg,
+        name="layer"
+    )
+    return layer(prev)
