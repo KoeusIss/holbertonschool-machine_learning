5fbba66f49cb119cb93e0cf222586c075edf0b7a
KoeusIss <sebri.issam@gmail.com>
2021-01-21 09:18:00 +0100

    tring some fix of the forward propagation

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -25,8 +25,10 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     """
     cache = {"A0": X}
     for el in range(1, L + 1):
-        Z = np.matmul(weights["W" + str(el)], cache["A" + str(el - 1)])
-        + weights["b" + str(el)]
+        Z = np.matmul(
+            weights["W" + str(el)],
+            cache["A" + str(el - 1)]
+            ) + weights["b" + str(el)]
         if el == L:
             t = np.exp(Z)
             A = t / np.sum(t, axis=0, keepdims=True)
