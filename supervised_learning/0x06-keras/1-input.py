64058679dd7ca19e26409b0834582ba8ea7b36bf
KoeusIss <sebri.issam@gmail.com>
2021-01-25 11:30:51 +0100

    pycodestyle fixes

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -6,17 +6,17 @@ import tensorflow.keras as keras
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """Builds a neural network with a keras library
-    
+
     Args:
         nx (int): Is the number of the input features of the network.
         layers (list): Is containing the number of nodes in each layers.
         activations (list): Is containing functions used for each layer.
         lambtha (float): Is the L2 regularization parameter.
         keep_prob (floa): Is the probability that a node will be kept.
-    
+
     Returns:
         keras.model: Returns the keras model
-    
+
     """
     inputs = keras.Input(shape=(nx,))
     regularizer = keras.regularizers.l2(lambtha)
