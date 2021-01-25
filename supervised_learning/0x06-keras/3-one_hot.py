48e04f3dcad3f29b8e7931b618ec0a62337f5635
KoeusIss <sebri.issam@gmail.com>
2021-01-25 11:28:18 +0100

    commit the one hot convertion

---
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Keras module"""
+import numpy as np
+
+
+def one_hot(labels, classes=None):
+    """Converts a label vector into a one-hot matrix
+
+    Args:
+        labels (numpy.ndarray): Is containing the input labels.
+        classes (int): number of classes
+
+    Returns:
+        numpy.ndarray: The one-hot matrix.
+
+    """
+    m = labels.shape[0]
+    if classes is None:
+        classes = labels.shape[0]
+    return np.eye(classes)[labels]
