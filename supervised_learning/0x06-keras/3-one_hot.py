7215fa3c28363b5dd4383ad30377f1b995769a61
KoeusIss <sebri.issam@gmail.com>
2021-01-26 08:44:16 +0100

    update the one-hot encoding with keras way

--- supervised_learning/0x06-keras/3-one_hot.py
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Keras module"""
-import numpy as np
+import tensorflow.keras as K
 
 
 def one_hot(labels, classes=None):
@@ -14,7 +14,4 @@ def one_hot(labels, classes=None):
         numpy.ndarray: The one-hot matrix.
 
     """
-    m = labels.shape[0]
-    if classes is None:
-        classes = len(np.unique(labels, axis=0))
-    return np.eye(classes)[labels]
+    return K.utils.to_categorical(labels, classes)
