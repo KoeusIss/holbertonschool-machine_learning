cb1ae1faaf41f5ef5c45d569343099fafee11b13
KoeusIss <sebri.issam@gmail.com>
2021-01-31 16:30:52 +0100

    update

--- math/0x04-convolutions_and_pooling/6-pool.py
+++ math/0x04-convolutions_and_pooling/6-pool.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def pool(images, kernel_shape, stride, mode='max'):
     """Performs pooling on images
-    
+
     Args:
         images (numpy.ndarray): Containing multiple images of shape
             (m, h, w, c) where m is the number of images, h is the height on
@@ -17,17 +17,17 @@ def pool(images, kernel_shape, stride, mode='max'):
             the stride of the height and sw is the stride of the width.
         mode (str): Indicates the type of pooling, `max` indicates max pooling
             `avg` indicates the average pooling
-    
+
     Returns:
         numpy.ndarray: Containing the pooled images
-    
+
     """
     m, input_h, input_w, c = images.shape
     kh, kw = kernel_shape
     sh, sw = stride
 
-    output_h = int(np.ceil(input_h / sh))
-    output_w = int(np.ceil(input_w / sw))
+    output_h = int(np.ceil((input_h - kh) / sh + 1))
+    output_w = int(np.ceil((input_w - kw) / sw + 1))
 
     output = np.zeros((m, output_h, output_w, c))
 
