497b9b824be3a6fa824c69287eb1bb68b5ec8bf1
KoeusIss <sebri.issam@gmail.com>
2021-02-03 15:29:16 +0100

    commit the keras implementation of lenet5

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -5,15 +5,15 @@ import tensorflow.keras as K
 
 def lenet5(X):
     """Builds a modified version of the `LeNet-5` architecture using Keras
-    
+
     Args:
         X (keras.input): Containing the input images for the network with shape
             (m, 28, 28, 1) where m is the number of images.
-    
+
     Returns:
         keras.model: A keras model compiled to use `Adam` optimization and
         accuracy mertics.
-    
+
     """
     C1 = K.layers.Conv2D(
         filters=6,
