2d7b0f32a9a30fda90c33593ed4376d85fa59374
KoeusIss <sebri.issam@gmail.com>
2021-03-01 12:27:24 +0100

    Add the alpha and beta number conditioon

--- supervised_learning/0x0C-neural_style_transfer/0-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/0-neural_style.py
@@ -42,9 +42,9 @@ class NST:
             raise TypeError(
                 'content_image must be a numpy.ndarray with shape (h, w, 3)'
             )
-        if alpha < 0:
+        if not isinstance(alpha, (int, float)) or alpha < 0:
             raise TypeError('alpha must be a non-negative number')
-        if beta < 0:
+        if not isinstance(beta, (int, float)) or beta < 0:
             raise TypeError('beta must be a non-negative number')
 
         tf.enable_eager_execution()
