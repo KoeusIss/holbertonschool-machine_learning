5b9e6452b85e5040a49ff84465b16b9daa9878cb
KoeusIss <sebri.issam@gmail.com>
2021-03-17 13:43:15 +0100

    commit the indefinitness task

---
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Advanced linear algebra module"""
+import numpy as np
+
+
+def definiteness(matrix):
+    """
+    Calculates the definetness of a matrix
+    """
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError('matrix must be a numpy.ndarray')
+
+    if any(len(row) != len(matrix) for row in matrix):
+        return None
+
+    if len(matrix.shape) < 2:
+        return None
+
+    eivals = np.lialg.eig(matrix)[0]
+    if all(eivals >= 0):
+        if any(eivals == 0):
+            return 'Positive semi-definite'
+        return 'Positive definite'
+    if all(eivals <= 0):
+        if any(eivals == 0):
+            return 'Negative semi-definite'
+        return 'Negative definite'
+    return 'Indefinite'
