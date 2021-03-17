aeb86216137391b95717edebb69f2e59eb2dc914
KoeusIss <sebri.issam@gmail.com>
2021-03-17 14:21:06 +0100

    commit for the symetric

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -9,6 +9,8 @@ def definiteness(matrix):
     """
     if not isinstance(matrix, np.ndarray):
         raise TypeError('matrix must be a numpy.ndarray')
+    if not np.all(matrix == matrix.T):
+        return None
     try:
         eivals = np.linalg.eig(matrix)[0]
         if all(eivals >= 0):
