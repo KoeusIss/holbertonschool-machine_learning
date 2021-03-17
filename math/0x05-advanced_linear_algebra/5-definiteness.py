f2ba95c11aad4c7da3c931e1aa6e81462eda7494
KoeusIss <sebri.issam@gmail.com>
2021-03-17 13:50:00 +0100

    wrap the function in try except

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -9,20 +9,16 @@ def definiteness(matrix):
     """
     if not isinstance(matrix, np.ndarray):
         raise TypeError('matrix must be a numpy.ndarray')
-
-    if any(len(row) != len(matrix) for row in matrix):
+    try:
+        eivals = np.linalg.eig(matrix)[0]
+        if all(eivals >= 0):
+            if any(eivals == 0):
+                return 'Positive semi-definite'
+            return 'Positive definite'
+        if all(eivals <= 0):
+            if any(eivals == 0):
+                return 'Negative semi-definite'
+            return 'Negative definite'
+        return 'Indefinite'
+    except Exception:
         return None
-
-    if len(matrix.shape) < 2:
-        return None
-
-    eivals = np.linalg.eig(matrix)[0]
-    if all(eivals >= 0):
-        if any(eivals == 0):
-            return 'Positive semi-definite'
-        return 'Positive definite'
-    if all(eivals <= 0):
-        if any(eivals == 0):
-            return 'Negative semi-definite'
-        return 'Negative definite'
-    return 'Indefinite'
