aae72b66fb37e5058b3a944d5c0b6063c5d067d2
KoeusIss <sebri.issam@gmail.com>
2021-03-25 13:02:53 +0100

    fix the number of events condition

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -51,7 +51,7 @@ def likelihood(x, n, P):
         numpy.ndarray: Containing the liklihood of obtaining the data x and n.
 
     """
-    if not isinstance(n, int) or n < 0:
+    if not isinstance(n, int) or n < 1:
         raise ValueError('n must be a positive integer')
     if not isinstance(x, int) or x < 0:
         raise ValueError(
