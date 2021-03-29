47c1726b81ecc7999446d1386943c41703f01109
KoeusIss <sebri.issam@gmail.com>
2021-03-29 11:20:06 +0100

    Update the initialize function

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -4,11 +4,14 @@ import numpy as np
 
 
 def initialize(X, k):
-    n, d = X.shape
-    X_min = np.min(X, axis=0)
-    X_max = np.max(X, axis=0)
-    return np.random.uniform(
-        low=X_min,
-        high=X_max,
-        size=(k, d)
-    )
+    try:
+        n, d = X.shape
+        X_min = np.min(X, axis=0)
+        X_max = np.max(X, axis=0)
+        return np.random.uniform(
+            low=X_min,
+            high=X_max,
+            size=(k, d)
+        )
+    except Exception:
+        return None
