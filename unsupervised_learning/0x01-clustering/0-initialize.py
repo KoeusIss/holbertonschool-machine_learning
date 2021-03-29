be60d5aa04ff1cef69613fa589e0ceb36ac30fe4
KoeusIss <sebri.issam@gmail.com>
2021-03-29 11:25:07 +0100

    Update the checking for number of clusters

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -4,14 +4,24 @@ import numpy as np
 
 
 def initialize(X, k):
-    try:
-        n, d = X.shape
-        X_min = np.min(X, axis=0)
-        X_max = np.max(X, axis=0)
-        return np.random.uniform(
-            low=X_min,
-            high=X_max,
-            size=(k, d)
-        )
-    except Exception:
+    """Initializes cluster centroids for K-means
+
+    Arguments:
+        X {numpy.ndarray} -- Containing the dataset
+        k {int} -- Indicate the number of cluster
+
+    Returns:
+        numpy.ndarray|None -- Containing the initialized centroids for each
+            dimension, Otherwise return None
+    """
+    if not isinstance(k, int) or k < 1:
         return None
+
+    n, d = X.shape
+    X_min = np.min(X, axis=0)
+    X_max = np.max(X, axis=0)
+    return np.random.uniform(
+        low=X_min,
+        high=X_max,
+        size=(k, d)
+    )
