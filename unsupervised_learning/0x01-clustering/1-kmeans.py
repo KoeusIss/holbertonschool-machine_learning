e783aa450eb87effec5100d996a76999869cbfc9
KoeusIss <sebri.issam@gmail.com>
2021-03-30 09:06:11 +0100

    call exactly twice the random uniform

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -3,27 +3,6 @@
 import numpy as np
 
 
-def initialize(X, k):
-    """Initializes cluster centroids for K-means
-
-    Arguments:
-        X {numpy.ndarray} -- Containing the dataset
-        k {int} -- Indicate the number of cluster
-
-    Returns:
-        numpy.ndarray|None -- Containing the initialized centroids for each
-            dimension, Otherwise return None
-    """
-    n, d = X.shape
-    X_min = np.min(X, axis=0)
-    X_max = np.max(X, axis=0)
-    return np.random.uniform(
-        low=X_min,
-        high=X_max,
-        size=(k, d)
-    )
-
-
 def get_closest(X, centroids):
     """Finds the closest points to centroids
 
@@ -52,22 +31,28 @@ def kmeans(X, k, iterations=1000):
         np.ndarray, np.ndarray -- The newly moved centroids, and the newly
         assigned classes
     """
-
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None
     if not isinstance(k, int) or k < 1:
         return None
-    if not isinstance(iterations, int) or iterations < 1:
-        return None
 
-    centroids = initialize(X, k)
+    n, d = X.shape
+    centroids = np.random.uniform(
+        low=np.min(X, axis=0),
+        high=np.max(X, axis=0),
+        size=(k, d)
+    )
 
     for i in range(iterations):
         old_centroids = np.copy(centroids)
         closest = get_closest(X, centroids)
         for j in range(k):
             if (X[closest == j].size == 0):
-                centroids[j] = initialize(X, 1)
+                centroids[j] = np.random.uniform(
+                    low=np.min(X, axis=0),
+                    high=np.max(X, axis=0),
+                    size=(1, d)
+                )
             else:
                 centroids[j] = np.mean(X[closest == j], axis=0)
         if np.all(old_centroids == centroids):
