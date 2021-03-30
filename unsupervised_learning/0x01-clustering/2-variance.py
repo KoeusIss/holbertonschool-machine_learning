15189085af95e450a18b5d7a832729d28b6fdae1
KoeusIss <sebri.issam@gmail.com>
2021-03-30 10:24:09 +0100

    wrap all the function inside try, except

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -15,8 +15,10 @@ def variance(X, C):
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None
-    if not isinstance(C, np.ndarray):
+    if not isinstance(C, np.ndarray) or len(C.shape) != 2:
         return None
-
-    distances = np.sqrt(np.sum((X - C[:, np.newaxis])**2, axis=2))
-    return np.sum(np.min(distances, axis=0)**2)
+    try:
+        distances = np.sqrt(np.sum((X - C[:, np.newaxis])**2, axis=2))
+        return np.sum(np.min(distances, axis=0)**2)
+    except Exception:
+        return None
