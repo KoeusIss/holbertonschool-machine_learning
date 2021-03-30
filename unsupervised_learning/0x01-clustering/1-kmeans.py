2a66f2736687ff2159357eacee1b664243238dde
KoeusIss <sebri.issam@gmail.com>
2021-03-30 09:22:00 +0100

    Forget about updating the class before returned it

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -59,4 +59,5 @@ def kmeans(X, k, iterations=1000):
                 centroids[j] = np.mean(X[closest == j], axis=0)
         if np.all(old_centroids == centroids):
             break
-    return centroids, closest
+
+    return centroids, get_closest(X, centroids)
