97205cf4f44148456f62fca87a298788ae326d0c
KoeusIss <sebri.issam@gmail.com>
2021-04-01 10:45:07 +0100

    update the rounding of the liklihood

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -7,7 +7,21 @@ maximization = __import__('7-maximization').maximization
 
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
-    """Performs the EM"""
+    """Perform the Expectation maximization for the GMM
+
+    Arguments:
+        X {np.ndarray} -- Containing the data points
+        k {int} -- Containing the number of class
+
+    Keyword Arguments:
+        iterations {int} -- Is the number of iterations (default: {1000})
+        tol {float} -- Is the tolearance over liklihood (default: {1e-5})
+        verbose {bool} -- Indicates if we could print status (default: {False})
+
+    Returns:
+        tuple(np.ndarray, float) -- the prior, the mean, the covaraince matrix
+        the posterior and the Liklihood
+    """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None, None
     if not isinstance(k, int) or k < 1:
@@ -24,15 +38,15 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     for it in range(iterations):
         old_L = L
         if verbose and (it % 10) == 0:
-            print('Log Likelihood after {i} iterations: {l:.5f}'.format(
-                i=it, l=L
+            print('Log Likelihood after {} iterations: {}'.format(
+                i=it, l=L.round(5)
             ))
         pi, m, S = maximization(X, g)
         g, L = expectation(X, pi, m, S)
         if abs(L - old_L) <= tol:
             break
     if verbose:
-        print('Log Likelihood after {i} iterations: {l:.5f}'.format(
-            i=it + 1, l=L
+        print('Log Likelihood after {} iterations: {}'.format(
+            i=it + 1, l=L.round(5)
         ))
     return pi, m, S, g, L
