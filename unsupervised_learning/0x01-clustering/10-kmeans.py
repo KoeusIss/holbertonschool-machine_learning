2a618e8388906c96f4d104776ca5fb965cfc1cf4
KoeusIss <sebri.issam@gmail.com>
2021-04-01 13:28:44 +0100

    add a documentation to the function

--- unsupervised_learning/0x01-clustering/10-kmeans.py
+++ unsupervised_learning/0x01-clustering/10-kmeans.py
@@ -4,6 +4,16 @@ import sklearn.cluster
 
 
 def kmeans(X, k):
+    """Performs K-means
+
+    Arguments:
+        X {np.ndarray} -- Containing the data points
+        k {int} -- Is the number of clusters
+
+    Returns:
+        tuple -- containing the centroids and the index for each data point
+        belongs.
+    """
     kmeans = sklearn.cluster.KMeans(k)
     model = kmeans.fit(X)
     return model.cluster_centers_, model.labels_
