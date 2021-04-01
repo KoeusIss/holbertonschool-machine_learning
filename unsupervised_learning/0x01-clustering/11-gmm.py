4f301feb0258adc5f1ab85d19ff2c7ab74ae5175
KoeusIss <sebri.issam@gmail.com>
2021-04-01 13:44:25 +0100

    commit the gmm with sklearn

--- unsupervised_learning/0x01-clustering/11-gmm.py
+++ unsupervised_learning/0x01-clustering/11-gmm.py
@@ -3,8 +3,17 @@ import sklearn.mixture
 
 
 def gmm(X, k):
+    """Performs the GMM
+
+    Arguments:
+        X {np.ndarray} -- Containing the data points
+        k {int} -- Is the number of clusters
+
+    Returns:
+        tuple -- priors, means, covariances, clss, bic
+    """
     _gmm = sklearn.mixture.GaussianMixture(k)
     model = _gmm.fit(X)
-    classes = _gmm.predict(X)
+    clss = _gmm.predict(X)
     bic = _gmm.bic(X)
-    return model.weights_, model.means_, model.covariances_, classes, bic
+    return model.weights_, model.means_, model.covariances_, clss, bic
