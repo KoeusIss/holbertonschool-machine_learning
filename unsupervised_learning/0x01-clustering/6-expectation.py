49129f995d205e611b8f59b9afe97b6434ef49c9
KoeusIss <sebri.issam@gmail.com>
2021-04-02 10:45:08 +0100

    update the checking

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -19,15 +19,20 @@ def expectation(X, pi, m, S):
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
-    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
-        return None, None
-    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
-        return None, None
-    if not isinstance(S, np.ndarray) or len(S.shape) != 3:
-        return None, None
 
     n, d = X.shape
+    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
+        return None, None
+
     k = pi.shape[0]
+    if k >= n:
+        return None, None
+
+    if not isinstance(m, np.ndarray) or m.shape != (k, d):
+        return None, None
+
+    if not isinstance(S, np.ndarray) or S.shape != (k, d, d):
+        return None, None
 
     nominator = np.zeros((k, n))
     for i in range(k):
