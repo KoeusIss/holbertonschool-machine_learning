d2a20a894bdcd531a7db11b19977ec8f90909acd
KoeusIss <sebri.issam@gmail.com>
2021-05-21 18:19:44 +0100

    commit the encoder block

--- supervised_learning/0x11-attention/7-transformer_encoder_block.py
+++ supervised_learning/0x11-attention/7-transformer_encoder_block.py
@@ -5,25 +5,56 @@ MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
 
 
 class EncoderBlock(tf.keras.layers.Layer):
+    """EncoderBlock class Creates an encoder block for a transformer
+    """
     def __init__(self, dm, h, hidden, drop_rate=0.1) -> None:
+        """Initializer
+
+        Arguments:
+            dm {int} -- Is a represention of the dimensionality of the model
+            h {int} -- Is a represention of the number of head
+            hidden {int} -- Is a representation of hidden fully connected layer
+
+        Keyword Arguments:
+            drop_rate {float} -- The drop rate (default: {0.1})
+        """
         super(EncoderBlock, self).__init__()
-        self.dm = dm
-        self.h = h
-        self.hidden = hidden
-        self.mha = MultiHeadAttention(self.dm, self.h)
+        self.mha = MultiHeadAttention(dm, h)
         self.dense_hidden = tf.keras.layers.Dense(
-            self.hidden,
+            hidden,
             activation='relu'
         )
-        self.dense_output = tf.keras.layers.Dense(self.dm)
+        self.dense_output = tf.keras.layers.Dense(dm)
         self.layernorm1 = tf.keras.layers.LayerNormalization(
             epsilon=1e-6
         )
         self.layernorm2 = tf.keras.layers.LayerNormalization(
             epsilon=1e-6
         )
+
         self.dropout1 = tf.keras.layers.Dropout(drop_rate)
         self.dropout2 = tf.keras.layers.Dropout(drop_rate)
 
-    call(self, x, training, mask=None):
+    def call(self, x, training, mask=None):
+        """Instance call
 
+        Arguments:
+            x {tf.Tensor} -- Is contains the input
+            training {boolean} -- Is a representation of training
+
+        Keyword Arguments:
+            mask {None} -- Always none (default: {None})
+
+        Returns:
+            tf.Tensor -- Block's output
+        """
+        attention, _ = self.mha(x, x, x, mask)
+        attention = self.dropout1(attention, training=training)
+        first = self.layernorm1(x + attention)
+
+        hidden = self.dense_hidden(first)
+        hidden = self.dense_output(hidden)
+        second = self.dropout2(hidden, training=training)
+
+        output = self.layernorm2(first + second)
+        return output
