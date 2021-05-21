1c326ee61caa438bf7e5273ae3067c694018c7f6
KoeusIss <sebri.issam@gmail.com>
2021-05-21 19:00:28 +0100

    commit the transformer decoder

--- supervised_learning/0x11-attention/8-transformer_decoder_block.py
+++ supervised_learning/0x11-attention/8-transformer_decoder_block.py
@@ -19,13 +19,13 @@ class DecoderBlock(tf.keras.layers.Layer):
             drop_rate {float} -- Drop rate (default: {0.1})
         """
         super(DecoderBlock, self).__init__()
-        self.mah1 = MultiHeadAttention(dm, h)
-        self.mah2 = MultiHeadAttention(dm, h)
+        self.mha1 = MultiHeadAttention(dm, h)
+        self.mha2 = MultiHeadAttention(dm, h)
         self.dense_hidden = tf.keras.layers.Dense(
             hidden,
             activation="relu"
         )
-        self.dense_ouput = tf.keras.layers.Dense(dm)
+        self.dense_output = tf.keras.layers.Dense(dm)
         self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.layernorm3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
@@ -46,18 +46,17 @@ class DecoderBlock(tf.keras.layers.Layer):
         Returns:
             tf.Tensor -- the decoder output
         """
-        first_attention, _ = self.mah1(x, x, x, look_ahead_mask)
+        first_attention, _ = self.mha1(x, x, x, look_ahead_mask)
         first_attention = self.dropout1(first_attention, training=training)
         first_output = self.layernorm1(x + first_attention)
 
-        second_attention, _ = self.mah2(
-            encoder_output, encoder_output, first_output, padding_mask)
+        second_attention, _ = self.mha2(
+            first_output, encoder_output, encoder_output, padding_mask)
         second_attention = self.dropout2(second_attention, training=training)
         second_output = self.layernorm2(second_attention + first_output)
 
         third_output = self.dense_hidden(second_output)
-        third_output = self.dense_ouput(third_output)
+        third_output = self.dense_output(third_output)
         third_output = self.dropout3(third_output, training=training)
-
         output = self.layernorm3(third_output + second_output)
         return output
