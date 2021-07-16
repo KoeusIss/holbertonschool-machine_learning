f50607f7cdbf820f98e70e8bf587655f623c9e94
KoeusIss <sebri.issam@gmail.com>
2021-07-16 21:44:33 +0100

    commit the policy gradient

--- reinforcement_learning/0x03-policy_gradients/policy_gradient.py
+++ reinforcement_learning/0x03-policy_gradients/policy_gradient.py
@@ -28,3 +28,35 @@ def policy(matrix, weight):
     """
     X = matrix @ weight
     return softmax(X)
+
+
+def softmax_grad(softmax):
+    """Computes the gradient of a given softmax
+
+    Arguments:
+        softmax {np.ndarray} -- Contains the softmax matrix.
+
+    Returns:
+        np.ndarray -- The gradient of the softmax
+    """
+    softmax = softmax.reshape(-1, 1)
+    return np.diagflat(softmax) - softmax @ softmax.T
+
+
+def policy_gradient(state, weight):
+    """Computes the Monte Carlo weighted policy gradient.
+
+    Arguments:
+        state {np.ndarray} -- Contains the states.
+        weight {np.ndarray} -- Contains Theta weights.
+
+    Returns:
+        tuple(np.ndarray) -- Contains the action and the gradient.
+    """
+    PI = policy(state, weight)
+    action = np.random.choice(len(PI[0]), p=PI[0])
+
+    d_PI = softmax_grad(PI)[action, :]
+    d_log = d_PI / PI[0, action]
+    grad = state.T @ d_log[None, :]
+    return action, grad
