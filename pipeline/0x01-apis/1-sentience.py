77608da3c7989ebd4314bd2b23ca659b4fc0dfb6
KoeusIss <sebri.issam@gmail.com>
2021-07-26 20:45:21 +0100

    update the loop

--- pipeline/0x01-apis/1-sentience.py
+++ pipeline/0x01-apis/1-sentience.py
@@ -14,15 +14,16 @@ def sentientPlanets():
     content = response.json()
 
     planets = list()
-    while content['next']:
+    while True:
         results = content['results']
-
         for sentinent in results:
             planet_url = sentinent['homeworld']
             if planet_url:
                 name = requests.get(planet_url).json()['name']
                 planets.append(name)
-
+        if content['next'] is None:
+            break
         response = requests.get(content['next'])
         content = response.json()
+
     return planets
