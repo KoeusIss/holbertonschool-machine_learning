e584b2f899cdee8a2468fdbea92f98a7e0c48fcf
KoeusIss <sebri.issam@gmail.com>
2021-08-02 20:19:34 +0100

    update the timestamp

--- pipeline/0x02-databases/16-glam_rock.sql
+++ pipeline/0x02-databases/16-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Wheres the Glams
-SELECT band_name, IFNULL(split, 2021) - formed as lifespan
+SELECT band_name, IFNULL(split, YEAR(CURRENT_TIMESTAMP)) - formed as lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
