a5d63e91f5ce7d285c41090d668acec1415bae97
KoeusIss <sebri.issam@gmail.com>
2021-08-02 22:28:24 +0100

    commit the average score

---
+++ pipeline/0x02-databases/20-average_score.sql
@@ -0,0 +1,15 @@
+-- Computes the average score for student
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+	SET @avg_score = (
+		SELECT AVG(score)
+		FROM corrections
+		WHERE corrections.user_id = user_id
+	);
+	UPDATE users
+	SET average_score = @avg_score WHERE id = user_id;
+END;
+//
+DELIMITER ;
