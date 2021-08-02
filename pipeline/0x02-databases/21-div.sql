7a35d70c4b44ccff349ec8d9f47158196a2ec5b3
KoeusIss <sebri.issam@gmail.com>
2021-08-02 22:42:29 +0100

    commit the divisor function

---
+++ pipeline/0x02-databases/21-div.sql
@@ -0,0 +1,19 @@
+-- Devides two value
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER //
+
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT
+
+BEGIN
+	DECLARE result FLOAT;
+	SET result = 0;
+
+	IF b <> 0 THEN
+		SET result = a / b;
+	END IF;
+
+	RETURN result;
+END;
+//
+DELIMITER ;
