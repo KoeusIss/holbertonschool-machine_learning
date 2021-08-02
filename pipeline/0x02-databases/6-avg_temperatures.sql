f57938ef4b724587347577cf912e835138dd17cb
KoeusIss <sebri.issam@gmail.com>
2021-08-02 11:38:24 +0100

    update the avg temp

--- pipeline/0x02-databases/6-avg_temperatures.sql
+++ pipeline/0x02-databases/6-avg_temperatures.sql
@@ -1,5 +1,5 @@
--- Grabs the temperature average per city
-SELECT city, AVG(value) AS avg_tmp
+-- Lists the avg of temperatures
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
-ORDER BY avg_tmp DESC;
+ORDER BY avg_temp DESC;
